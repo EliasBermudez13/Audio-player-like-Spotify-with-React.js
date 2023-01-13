ad33b92c6d56b08c3871383e75b6942990020a4b
Elias Bermudez <eliasbermudezxiii@gmail.com>
2023-01-13 14:12:16 +0000

    repeat song functionality

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -5,6 +5,7 @@ const Home = () => {
 	const [playList, setplayList] = useState([]);
 	const [iconStatus, setIconStatus] = useState("fa fa-play rounded-circle");
 	let [position, setposition] = useState(0);
+	const [repeat, setrepeat] = useState(false);
 	let songUrl = useRef();
 	useEffect(() => {
 		fetch("https://assets.breatheco.de/apis/sound/songs")
@@ -58,19 +59,29 @@ const Home = () => {
 		songUrl.current.volume -= 0.10;
 	}
 
+	const repeatSong = () => {
+		if (repeat === true){
+			setrepeat(false);
+		} else{
+			setrepeat(true);
+		}
+	}
+
 	return (
 			<div className="container-text-center">
 				<ul className="list-group group-flash h-auto">
 					{playList.map((song, index) => <button className="btn btn-dark text-start rounded-0 border border-secondary border-opacity-25" type="button" key={index} onClick={() => songSelect(song.url, index)}>{index} {song.name} -</button>)}
 				</ul>
 				<div className= "d-flex justify-content-center bg-secondary bg-black sticky-bottom">
-				<audio id="reproductor" ref={songUrl}></audio>
-				<button className="m-2 bg-black text-white" style= {{height: "30px"}} onClick={(bajarVolumen)}><i class="fas fa-volume-down mx-2" style= {{widgh: "50%", height: "25%"}}></i></button>
+				<audio id="reproductor" ref={songUrl} loop={repeat}></audio>
+				<button className="m-2 bg-black text-white" style= {{height: "30px"}} onClick={(bajarVolumen)}><i className="fas fa-volume-down mx-2" style= {{widgh: "50%", height: "25%"}}></i></button>
 				<button className="m-2 bg-black text-white" style= {{height: "30px"}} onClick={(previousSong)}><i className="fa fa-backward mx-2" style= {{widgh: "50%", height: "25%"}}></i></button>
 				<button className="m-2 bg-black text-white" style= {{height: "30px"}} onClick={(changeIcon)}><i className={iconStatus} style= {{widgh: "50%", height: "25%"}}></i></button>
 				<button className="m-2 bg-black text-white" style= {{height: "30px"}} onClick={(nextSong)}><i className="fa fa-forward mx-2" style= {{widgh: "50%", height: "25%"}}></i></button>
+				<button className="m-2 bg-black text-white" style= {{height: "30px"}} onClick={(subirVolumen)}><i className="mx-2 fas fa-volume-up" style= {{widgh: "50%", height: "25%"}}></i></button>
 				
-				<button className="m-2 bg-black text-white" style= {{height: "30px"}} onClick={(subirVolumen)}><i class="mx-2 fas fa-volume-up" style= {{widgh: "50%", height: "25%"}}></i></button>
+				<input id="repeat" type="checkbox" checked={repeat} onChange={repeatSong}/>
+				<label className="mx-2 m-2 text-white" htmlFor="repeat"> Repeat</label>
 			</div>
 		</div>
 	);
